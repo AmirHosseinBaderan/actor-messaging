86382f7c13b0adb2d0c197f53b5d64f6fce7ce94
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-09-11 11:29:18 +0330

    create : message actor

---
+++ ActorMessaging/Src/Server/ActorMessaging.Server/Actor/Message.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+
+namespace ActorMessaging.Server.Actor;
+
+public class MessageActor : ReceiveActor
+{
+    public MessageActor()
+    {
+        Receive<string>(HandleMessage);
+    }
+
+    public static void HandleMessage(string message)
+    {
+        Console.WriteLine($"Client send : {message}");
+    }
+}
