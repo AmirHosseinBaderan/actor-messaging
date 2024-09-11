0f5d749e40ddcc8e19ee9cbb55003b28e8b2dfbf
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-09-11 11:29:28 +0330

    update : implement server and client

--- ActorMessaging/Src/Client/ActorMessaging.Client/Program.cs
+++ ActorMessaging/Src/Client/ActorMessaging.Client/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Akka.Actor;
+using Akka.Configuration;
+
+var config = ConfigurationFactory.ParseString(@"
+            akka {
+                actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                remote.dot-netty.tcp {
+                    hostname = ""localhost""
+                    port = 0
+                }
+            }");
+
+ActorSystem system = ActorSystem.Create(ClientName, config);
+ActorSelection actorSelection = system.ActorSelection($"akka.tcp://{ServerName}@{Host}:{Port}/user/{MessageActorName}");
+
+while (true)
+{
+    Write("Please write your message : ");
+    string? message = ReadLine();
+    if (message == "exit")
+    {
+        WriteLine("Finish Thank your :)");
+        break;
+    }
+
+    actorSelection.Tell(message);
+}
--- ActorMessaging/Src/Server/ActorMessaging.Server/Program.cs
+++ ActorMessaging/Src/Server/ActorMessaging.Server/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ActorMessaging.Server.Actor;
+using Akka.Actor;
+using Akka.Configuration;
+
+
+
+Config config = ConfigurationFactory.ParseString(@"
+            akka {
+                actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                remote.dot-netty.tcp {
+                    hostname = ""localhost""
+                    port = 8081
+                }
+            }");
+
+using ActorSystem system = ActorSystem.Create(ServerName, config);
+var actor = system.ActorOf(Props.Create(() => new MessageActor()), MessageActorName);
+
+WriteLine($"Server started at : {Host}:{Port} \n Press any key to stop server :)");
+ReadLine();
